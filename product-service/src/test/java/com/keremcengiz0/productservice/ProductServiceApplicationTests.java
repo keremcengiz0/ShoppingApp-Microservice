15f6d08c3c818f8fcce203dc28a0dd8c51e7f1a2
keremcengiz0 <keremcengiz0@hotmail.com>
2023-03-30 14:03:55 +0300

    fixed bug.

--- product-service/src/test/java/com/keremcengiz0/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/keremcengiz0/productservice/ProductServiceApplicationTests.java
@@ -1,12 +1,15 @@
 package com.keremcengiz0.productservice;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.keremcengiz0.productservice.dto.ProductCreateRequest;
+import com.keremcengiz0.productservice.dto.ProductResponse;
 import com.keremcengiz0.productservice.model.Product;
 import com.keremcengiz0.productservice.repository.ProductRepository;
 import com.keremcengiz0.productservice.service.ProductService;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +58,12 @@ class ProductServiceApplicationTests {
 		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 
+
+	@BeforeEach
+	void setUp() {
+		productRepository.deleteAll();
+	}
+
 	@Test
 	void shouldCreateProduct() throws Exception {
 		ProductCreateRequest productCreateRequest = getProductCreateRequest();
@@ -77,21 +86,35 @@ class ProductServiceApplicationTests {
 	}
 
 	@Test
-	void shouldReturnAllProducts() throws Exception {
+	void shouldGetAllProducts() throws Exception {
+		Product product1 = new Product();
+		product1.setName("iphone 13");
+		product1.setDescription("iphone 13");
+		product1.setPrice(BigDecimal.valueOf(1200));
+		productRepository.save(product1);
 
-		List<Product> productList = Arrays.asList(
-				new Product("64242b06cb8a140b045afa84", "Xiaomi", "mi 11 lite", BigDecimal.valueOf(1000)),
-				new Product("64242b06cb8a140b045afa85", "Samsung", "S22", BigDecimal.valueOf(900))
-		);
+		Product product2 = new Product();
+		product2.setName("Samsung Galaxy S21");
+		product2.setDescription("Samsung Galaxy S21");
+		product2.setPrice(BigDecimal.valueOf(1000));
+		productRepository.save(product2);
 
-		productRepository.saveAll(productList);
+		List<ProductResponse> expectedProducts = Arrays.asList(
+				modelMapper.map(product1, ProductResponse.class),
+				modelMapper.map(product2, ProductResponse.class));
 
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/product")
+		String responseString = mockMvc.perform(MockMvcRequestBuilders.get("/api/product")
 						.contentType(MediaType.APPLICATION_JSON))
-						.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				.andReturn().getResponse().getContentAsString();
 
-			Assertions.assertEquals(2, productRepository.findAll().size());
+		List<ProductResponse> actualProducts = objectMapper.readValue(responseString, new TypeReference<List<ProductResponse>>() {});
 
+		Assertions.assertEquals(expectedProducts.size(), actualProducts.size());
+
+		for (int i = 0; i < expectedProducts.size(); i++) {
+			Assertions.assertEquals(expectedProducts.get(i), actualProducts.get(i));
+		}
 	}
 
 }
