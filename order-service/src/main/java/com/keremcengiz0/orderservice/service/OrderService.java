1111f5878c2e5899af6045d0540d32380b0774ac
keremcengiz0 <keremcengiz0@hotmail.com>
2023-03-30 12:41:44 +0300

    added inventory-service codes.

--- order-service/src/main/java/com/keremcengiz0/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/keremcengiz0/orderservice/service/OrderService.java
@@ -7,6 +7,7 @@ import com.keremcengiz0.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final ModelMapper modelMapper;
